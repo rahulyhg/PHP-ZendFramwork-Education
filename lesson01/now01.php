20b6cf55065a765e7c72784780518af9185d7661
leeivan <leeivan2008@gmail.com>
2014-01-06 15:34:50 -0800

    update

---
+++ lesson01/now01.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>My first PHP page</title>
+
+</head>
+<body>
+
+	<?php
+	echo date ( "r" );
+	
+	?>
+
+	</body>
+</html>
---
+++ lesson01/now02.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>My first PHP document</title>
+</head>
+<body>
+
+	<?php
+	echo "<p>Current date and time: " . date ( "r" ) . "</p>";
+	
+	?>
+
+	</body>
+</html>
