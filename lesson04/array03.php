c46ab1c853dc25c848a499e6135410366a852c9d
leeivan <leeivan2008@gmail.com>
2014-01-15 22:38:31 -0800

    update

---
+++ lesson04/array03.php
@@ -0,0 +1,14 @@
+<?php
+$array = array (
+		"foo" => "bar",
+		"bar" => "foo" 
+);
+
+// 自 PHP 5.4 起
+// $array = [ 
+// 		"foo" => "bar",
+// 		"bar" => "foo" 
+// ];
+echo "hello" . $array['foo'];
+// echo phpinfo();
+?>
